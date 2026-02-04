bccf988ae544cbb5889892886d1f7f7e026aa630
playerz0redd <ipasha1337@yandex.by>
2026-02-05 01:10:09 +0300

    Initial Commit

---
+++ game-catalog/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  game-catalog
+//
+//  Created by Pavel Playerz0redd on 5.02.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ game-catalog/game_catalogApp.swift
@@ -0,0 +1,17 @@
+//
+//  game_catalogApp.swift
+//  game-catalog
+//
+//  Created by Pavel Playerz0redd on 5.02.26.
+//
+
+import SwiftUI
+
+@main
+struct game_catalogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

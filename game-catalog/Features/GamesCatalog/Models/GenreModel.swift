536358c1bd802315287de64573b0141edd7c28f7
playerz0redd <ipasha1337@yandex.by>
2026-02-06 04:29:15 +0300

    [ADDED] Added genres model

--- game-catalog/Features/GamesCatalog/Models/GenreModel.swift
+++ game-catalog/Features/GamesCatalog/Models/GenreModel.swift
@@ -5,14 +5,7 @@
 //  Created by Pavel Playerz0redd on 6.02.26.
 //
 
-import SwiftUI
-
-struct GenreModel: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
-
-#Preview {
-    GenreModel()
+struct GenreModel: Decodable, Identifiable {
+    let id: Int
+    let name: String
 }

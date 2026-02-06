2f730f03ce8291151f88ede4aa5f354c6685aa60
playerz0redd <ipasha1337@yandex.by>
2026-02-06 04:36:27 +0300

    [ADDED] Added logic to games catalog vm

--- game-catalog/Features/GamesCatalog/ViewModels/GamesCatalogViewModel.swift
+++ game-catalog/Features/GamesCatalog/ViewModels/GamesCatalogViewModel.swift
@@ -6,3 +6,56 @@
 //
 
 import Foundation
+import Combine
+import UIKit
+
+final class GamesCatalogViewModel: ObservableObject {
+    @Published var games: [GameModel] = []
+    @Published var isLoading = false
+    @Published var isShowingGenres = false
+    @Published var genres: [GenreModel] = []
+    @Published var selectedGenre: String? 
+    
+    let gamesCatalogService: IGamesCatalogService
+    
+    init(gamesCatalogService: IGamesCatalogService) {
+        self.gamesCatalogService = gamesCatalogService
+        loadMoreGames()
+        getGenres()
+    }
+    
+    func loadMoreGames() {
+        guard !isLoading else { return }
+        isLoading = true
+        
+        Task(priority: .userInitiated) {
+            let response = try await gamesCatalogService.fetchGamesList(genre: selectedGenre)
+            let newGames = response.results
+            
+            let urls = newGames.compactMap({ $0.backgroundImage })
+            
+            gamesCatalogService.prefetchImages(urls: urls)
+            
+            await MainActor.run {
+                self.games += newGames
+                isLoading = false
+            }
+        }
+    }
+    
+    func getGenres() {
+        Task {
+            let response = try await gamesCatalogService.getGenres()
+            self.genres = response.results
+        }
+    }
+
+    func onGenreSelect(genre: String) {
+        selectedGenre = genre
+        games = []
+        loadMoreGames()
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.25) {
+            self.isShowingGenres = false
+        }
+    }
+}

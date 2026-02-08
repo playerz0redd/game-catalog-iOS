a3b68e3300dcba6a10c774a1cf6dec95b5ddd9ab
playerz0redd <ipasha1337@yandex.by>
2026-02-08 20:32:27 +0300

    [ADDED] Added app coordinator

--- game-catalog/App/AppCoordinator/AppCoordinator.swift
+++ game-catalog/App/AppCoordinator/AppCoordinator.swift
@@ -5,4 +5,39 @@
 //  Created by Pavel Playerz0redd on 8.02.26.
 //
 
-import Foundation
+import SwiftUI
+import Combine
+
+final class AppCoordinator: ObservableObject {
+    
+    @Published var path = NavigationPath()
+    
+    private let networkManager: INetworkManager
+    private let gamesService: IGamesCatalogService
+    private(set) lazy var catalogViewModel = GamesCatalogViewModel(gamesCatalogService: gamesService)
+    
+    init() {
+        networkManager = NetworkManager()
+        gamesService = GamesCatalogService(networkManager: networkManager)
+    }
+    
+    func push(route: AppRouter) {
+        self.path.append(route)
+    }
+    
+    func pop() {
+        self.path.removeLast()
+    }
+    
+    
+    @ViewBuilder func getView(route: AppRouter) -> some View {
+        
+        switch route {
+        case .catalog:
+            GamesCatalogView(viewModel: catalogViewModel)
+        case .details(let gameId):
+            let vm = GameDetailsViewModel(gameId: gameId, gamesService: gamesService)
+            GameDetailsView(viewModel: vm)
+        }
+    }
+}

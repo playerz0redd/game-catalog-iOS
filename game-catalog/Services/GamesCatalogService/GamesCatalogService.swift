3dd179432dd60a62801b2eec33e6d249634447e0
playerz0redd <ipasha1337@yandex.by>
2026-02-08 20:34:28 +0300

    [ADDED] Added new methods to fetch details to game service

--- game-catalog/Services/GamesCatalogService/GamesCatalogService.swift
+++ game-catalog/Services/GamesCatalogService/GamesCatalogService.swift
@@ -15,6 +15,10 @@ protocol IGamesCatalogService {
     func prefetchImages(urls: [String])
     func clearCache()
     func getGenres() async throws -> GamesApiResponse<[GenreModel]>
+    func getGameDetails(gameId: Int) async throws -> GameDetailsModel
+    func getGameStores(gameId: Int) async throws -> GamesApiResponse<[GameInStoresModel]?>
+    func getGameVideos(gameId: Int) async throws -> GamesApiResponse<[GameVideoModel]?>
+    func getGameScreenshots(gameId: Int) async throws -> GamesApiResponse<[ScreenshotModel]?> 
     
 }
 
@@ -44,19 +48,40 @@ final class GamesCatalogService: IGamesCatalogService {
     ) async throws -> GamesApiResponse<[GameModel]> {
         do {
             let data = try await networkManager.fetch(
-                url: ListGamesEndpoints.listGames(page: currentPage, genre: genre, search: search).url
+                url: GameApiEndpoints.listGames(page: currentPage, genre: genre, search: search).url
             )
             currentPage += 1
             return try DecodeManager.decode(data: data, as: GamesApiResponse.self)
         } catch let error {
+            print(error.localizedDescription)
             throw error
         }
     }
     
     func getGenres() async throws -> GamesApiResponse<[GenreModel]> {
+        try await performApiRequest(endpoint: GameApiEndpoints.genres(page: 1), type: GamesApiResponse<[GenreModel]>.self)
+    }
+    
+    func getGameDetails(gameId: Int) async throws -> GameDetailsModel {
+        try await performApiRequest(endpoint: GameApiEndpoints.gameDetails(gameId: gameId), type: GameDetailsModel.self)
+    }
+    
+    func getGameStores(gameId: Int) async throws -> GamesApiResponse<[GameInStoresModel]?> {
+        try await performApiRequest(endpoint: GameApiEndpoints.gameStores(gameId: gameId), type: GamesApiResponse<[GameInStoresModel]?>.self)
+    }
+    
+    func getGameVideos(gameId: Int) async throws -> GamesApiResponse<[GameVideoModel]?> {
+        try await performApiRequest(endpoint: GameApiEndpoints.gameVideos(gameId: gameId), type: GamesApiResponse<[GameVideoModel]?>.self)
+    }
+    
+    func getGameScreenshots(gameId: Int) async throws -> GamesApiResponse<[ScreenshotModel]?> {
+        try await performApiRequest(endpoint: GameApiEndpoints.gameScreenshots(gameId: gameId), type: GamesApiResponse<[ScreenshotModel]?>.self)
+    }
+    
+    private func performApiRequest<T: Decodable>(endpoint: GameApiEndpoints, type: T.Type) async throws -> T {
         do {
-            let data = try await networkManager.fetch(url: ListGamesEndpoints.genres(page: 1).url)
-            return try DecodeManager.decode(data: data, as: GamesApiResponse<[GenreModel]>.self)
+            let data = try await networkManager.fetch(url: endpoint.url)
+            return try DecodeManager.decode(data: data, as: type.self)
         } catch let error {
             throw error
         }

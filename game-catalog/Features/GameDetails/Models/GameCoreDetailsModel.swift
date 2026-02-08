9a1375802e9a7cd44680d4a17d0a4a8486650fa9
playerz0redd <ipasha1337@yandex.by>
2026-02-08 20:30:19 +0300

    [Added] Added union model of game details

--- game-catalog/Features/GameDetails/Models/GameCoreDetailsModel.swift
+++ game-catalog/Features/GameDetails/Models/GameCoreDetailsModel.swift
@@ -6,3 +6,10 @@
 //
 
 import Foundation
+
+struct GameCoreDetailsModel {
+    let details: GameDetailsModel
+    let screenshots: [ScreenshotModel]?
+    let videos: [GameVideoModel]?
+    let stores: [GameInStoresModel]?
+}

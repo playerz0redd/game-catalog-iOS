b07079e6d2e9e55cedb5343bcfc0918e9fcaa10c
playerz0redd <ipasha1337@yandex.by>
2026-02-08 15:48:19 +0300

    [ADDED] Added video model

--- game-catalog/Features/GameDetails/Models/VideoModel.swift
+++ game-catalog/Features/GameDetails/Models/VideoModel.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+
+struct GameVideoModel: Decodable {
+    let id: Int
+    let name: String
+    let preview: String
+    let videos: VideoQualityUrls
+    
+    
+    struct VideoQualityUrls: Decodable {
+        let low: String?
+        let high: String?
+        
+        enum CodingKeys: String, CodingKey {
+            case low = "480"
+            case high = "max"
+        }
+    }
+}

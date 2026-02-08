26e2510147498f88c766233fa78ec7f68c73bb2f
playerz0redd <ipasha1337@yandex.by>
2026-02-08 20:35:33 +0300

    [REFACTOR] Decompositing game cell view

--- game-catalog/Features/GamesCatalog/Views/GameCell.swift
+++ game-catalog/Features/GamesCatalog/Views/GameCell.swift
@@ -19,28 +19,38 @@ struct GameCell: View {
     var body: some View {
 
         VStack(alignment: .leading, spacing: 8) {
-
-                Color.gray.opacity(0.3)
-                    .aspectRatio(1, contentMode: .fill)
-                    .overlay(
-                        KFImage(URL(string: game.backgroundImage ?? ""))
-                            .placeholder { ProgressView() }
-                            .onFailure { error in print("Ошибка: \(error)") }
-                            .setProcessor(DownsamplingImageProcessor(size: CGSize(width: 400, height: 400)))
-                            .cacheMemoryOnly(false)
-                            .fade(duration: 0.25)  
-                            .resizable()
-                            .scaledToFill()
-                    )
-                    .clipped()
-                    .clipShape(RoundedRectangle(cornerRadius: 12))
-                
+            imageView
             
-            Text(game.name)
-                .font(.caption)
-                .fontWeight(.medium)
-                .lineLimit(1)
+            textLabel
         }
+        
+    }
+}
+
+private extension GameCell {
+    
+    var imageView: some View {
+        Color.gray.opacity(0.3)
+            .aspectRatio(1, contentMode: .fill)
+            .overlay(
+                KFImage(URL(string: game.backgroundImage ?? ""))
+                    .placeholder { ProgressView() }
+                    .onFailure { error in print("Ошибка: \(error)") }
+                    .setProcessor(DownsamplingImageProcessor(size: CGSize(width: 400, height: 400)))
+                    .cacheMemoryOnly(false)
+                    .fade(duration: 0.25)
+                    .resizable()
+                    .scaledToFill()
+            )
+            .clipped()
+            .clipShape(RoundedRectangle(cornerRadius: 12))
+    }
+    
+    var textLabel: some View {
+        Text(game.name)
+            .font(.caption)
+            .fontWeight(.medium)
+            .lineLimit(1)
     }
 }
 

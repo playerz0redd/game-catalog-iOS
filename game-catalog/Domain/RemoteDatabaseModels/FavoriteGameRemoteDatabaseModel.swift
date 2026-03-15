b45ccef98577ae8bee734ede050e053246a36a6c
pavel.bychkouski <ipasha1337@yandex.by>
2026-03-15 23:37:15 +0300

    [ADDED] Added favorite game model for remote database

--- game-catalog/Domain/RemoteDatabaseModels/FavoriteGameRemoteDatabaseModel.swift
+++ game-catalog/Domain/RemoteDatabaseModels/FavoriteGameRemoteDatabaseModel.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+import FirebaseFirestore
+
+struct FavoriteGameRemoteDatabaseModel: Codable, Identifiable {
+    let id: Int
+    let name: String
+    let imagePath: String
+}
+
+extension FavoriteGameRemoteDatabaseModel {
+    
+    init(from model: FavoriteModel) {
+        self.id = model.id
+        self.name = model.name
+        self.imagePath = model.imagePath
+    }
+    
+    init(from model: GameDetailsModel) {
+        self.id = model.id
+        self.name = model.name
+        self.imagePath = model.imageUrl
+    }
+    
+    func toFavoritesModel(image: Data) -> FavoriteModel {
+        .init(id: self.id, name: self.name, image: image, imagePath: self.imagePath)
+    }
+}

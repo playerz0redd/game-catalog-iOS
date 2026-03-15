3845071c9378d186b04e9b921253500ce9b0f11e
pavel.bychkouski <ipasha1337@yandex.by>
2026-03-15 23:30:04 +0300

    [ADDED] Added own errors for network manager

--- game-catalog/Core/NetworkManager/NetworkError.swift
+++ game-catalog/Core/NetworkManager/NetworkError.swift
@@ -7,8 +7,32 @@
 
 import Foundation
 
-enum NetworkError: Error {
+enum NetworkError: Equatable, Sendable {
+    
+    nonisolated static func == (lhs: NetworkError, rhs: NetworkError) -> Bool {
+        switch (lhs, rhs) {
+        case (.urlError, .urlError), (.requestError, .requestError), (.serverError, .serverError): true
+        default: false
+        }
+    }
+    
     case urlError
     case requestError(Error)
     case serverError(code: Int)
+}
+
+extension NetworkError: IAppError {
+    var errorDescription: LocalizedStringResource {
+        switch self {
+        case .urlError:                      "Server url error"
+        case .requestError(let error):       "Error requesting server: \(error.localizedDescription)"
+        case .serverError(let code):         "Server error with code \(code)"
+        }
+    }
+    
+    var needsRefresh: Bool {
+        true
+    }
+    
+    
 }

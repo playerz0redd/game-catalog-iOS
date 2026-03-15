2b9f74842b6d57e71c228c68147b52a191e34df6
pavel.bychkouski <ipasha1337@yandex.by>
2026-03-15 23:35:00 +0300

    [ADDED] Added own errors for auth service

--- game-catalog/Services/AuthService/AuthServiceError.swift
+++ game-catalog/Services/AuthService/AuthServiceError.swift
@@ -7,8 +7,75 @@
 
 import Foundation
 
-enum AuthServiceError: Error {
-    case registrationError(AuthExceptions)
-    case singInError(AuthExceptions)
-    case signOutError(AuthExceptions)
+enum AuthServiceError: Equatable, Sendable {
+    
+    nonisolated static func ==(lhs: AuthServiceError, rhs: AuthServiceError) -> Bool {
+        switch (lhs, rhs) {
+        case (.registrationError, .registrationError), (.signInError, .signInError), (.signOutError, .signOutError), (.nameValidationError, .nameValidationError), (.emailValidationError, .emailValidationError), (.passwordValidationError, .passwordValidationError): true
+        default: false
+        }
+    }
+    
+    case registrationError(AuthError)
+    case signInError(AuthError)
+    case signOutError(AuthError)
+    case nameValidationError(ValidationNameError)
+    case emailValidationError
+    case passwordValidationError(ValidationPasswordError, AuthViewModel.AuthField)
+    
+    enum ValidationPasswordError: Error {
+        
+        case tooShort
+        case tooLong
+        case passwordsAreNotEqual
+        case empty
+        
+        var description: LocalizedStringResource {
+            switch self {
+            case .tooShort:                     "Password is too short"
+            case .tooLong:                      "Password is too long"
+            case .passwordsAreNotEqual:         "Passwords are not equal"
+            case .empty:                        "Password is empty"
+            }
+        }
+    }
+    
+    enum ValidationNameError: Error {
+        case tooShort
+        case tooLong
+        case empty
+        
+        var description: LocalizedStringResource {
+            switch self {
+            case .tooShort:          "Name is too short"
+            case .tooLong:           "Name is too long"
+            case .empty:             "Name is empty"
+            }
+        }
+    }
+    
+}
+
+extension AuthServiceError: IAppError {
+    var errorDescription: LocalizedStringResource {
+        switch self {
+        case .registrationError(let authError):                        authError.errorDescription
+        case .signInError(let authError):                              authError.errorDescription
+        case .signOutError(let authError):                             authError.errorDescription
+        case .nameValidationError(let nameError):                      nameError.description
+        case .emailValidationError:                                    "Email is not correct"
+        case .passwordValidationError(let passwordError, let field):   passwordError.description
+        }
+    }
+    
+    var needsRefresh: Bool {
+        switch self {
+        case .registrationError(let authError):      authError.needsRefresh
+        case .signInError(let authError):            authError.needsRefresh
+        case .signOutError(let authError):           authError.needsRefresh
+        case .nameValidationError, .emailValidationError, .passwordValidationError: true
+        }
+    }
+    
+    
 }

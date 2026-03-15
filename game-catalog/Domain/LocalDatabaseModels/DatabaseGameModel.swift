3dae9a57a64354af612ad85fc2f6b80db7228ead
pavel.bychkouski <ipasha1337@yandex.by>
2026-03-15 23:36:42 +0300

    [ADDED] Added game model for local database

--- game-catalog/Domain/LocalDatabaseModels/DatabaseGameModel.swift
+++ game-catalog/Domain/LocalDatabaseModels/DatabaseGameModel.swift
@@ -13,22 +13,28 @@ class DatabaseGameModel {
     var id: Int
     var name: String
     @Attribute(.externalStorage) var image: Data
+    var imagePath: String
+    var uid: String
     
-    init(id: Int, name: String, image: Data) {
+    init(id: Int, name: String, image: Data, imagePath: String, uid: String) {
         self.id = id
         self.name = name
         self.image = image
+        self.imagePath = imagePath
+        self.uid = uid
     }
     
-    init(from: GameDetailsModel, image: Data) {
+    init(from: GameDetailsModel, image: Data, uid: String) {
         self.id = from.id
         self.name = from.name
         self.image = image
+        self.imagePath = from.imageUrl
+        self.uid = uid
     }
 }
 
 extension DatabaseGameModel {
     func toFavoritesModel() -> FavoriteModel {
-        .init(id: self.id, name: self.name, image: self.image)
+        .init(id: self.id, name: self.name, image: self.image, imagePath: self.imagePath)
     }
 }
